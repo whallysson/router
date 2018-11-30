b54cb2a2cae0302cd40db2cc44d2e5f651edb15c
Whallysson Avelino <whallyssonallain@gmail.com>
2018-11-30 16:59:33 -0300

    Atualização das classes

--- src/Router.php
+++ src/Router.php
@@ -1,10 +1,61 @@
 <?php
 
+namespace CodeBlog\Router;
+
 /**
- * Router [ TIPO ]
- * Description
- * @copyright (c) year, Whallysson Allain AGÊNCIA GOOUP
+ * Class CodeBlog Router
+ *
+ * @author Whallysson Avelino <https://github.com/whallysson>
+ * @package CodeBlog\Router
  */
-class Router {
-    //put your code here
+class Router extends Dispatch {
+    /**
+     * Router constructor.
+     *
+     * @param string $projectUrl
+     * @param null|string $separator
+     */
+    public function __construct(string $projectUrl, ?string $separator = ":") {
+        parent::__construct($projectUrl, $separator);
+    }
+
+    /**
+     * @param string $route
+     * @param string|callable $handler
+     */
+    public function post(string $route, $handler): void {
+        $this->add("POST", $route, $handler);
+    }
+
+    /**
+     * @param string $route
+     * @param string|callable $handler
+     */
+    public function get(string $route, $handler): void {
+        $this->add("GET", $route, $handler);
+    }
+
+    /**
+     * @param string $route
+     * @param string|callable $handler
+     */
+    public function put(string $route, $handler): void {
+        $this->add("PUT", $route, $handler);
+    }
+
+    /**
+     * @param string $route
+     * @param string|callable $handler
+     */
+    public function patch(string $route, $handler): void {
+        $this->add("PATCH", $route, $handler);
+    }
+
+    /**
+     * @param string $route
+     * @param string|callable $handler
+     */
+    public function delete(string $route, $handler): void {
+        $this->add("DELETE", $route, $handler);
+    }
 }
